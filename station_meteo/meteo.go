a92ccd45a33b4d5f17cf0380a418893c50282d38
hdagbegn <dagbegnonedvige.houmasse@ynov.com>
2025-09-22 15:45:12 +0200

    function pour veifier le minimum temperature entre matin et soir

---
+++ station_meteo/meteo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func MinimalTemperature(technicien_name string , temperature_matin int8, temperature_soir int8){
+	if temperature_matin < temperature_soir {
+		fmt.Printf("la temperature minimal est: %d , le techicien en charge est : %s",temperature_matin, technicien_name)
+	}else if temperature_matin > temperature_soir {
+		fmt.Printf("la temperature minimal est: %d , le techicien en charge est : %s",temperature_soir, technicien_name)
+	}
+}
+
+func main(){
+	var TM  int8
+	var TS  int8
+	var nom string 
+	fmt.Println("entre ton nom : ")
+	fmt.Scanln(&nom)
+	fmt.Println("entre ton année LA TEMPERATURE DU MATIN: ")
+	fmt.Scanln(&TM)
+	fmt.Println("entre ton année LA TEMPERATURE DU SOIR: ")
+	fmt.Scanln(&TS)
+	MinimalTemperature(nom, TM, TS)
+}
